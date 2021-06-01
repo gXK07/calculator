7a2d880abe1b340756a333531a52d39d168a77d5
Greg <gregoire.ansard@gmail.com>
2021-06-01 16:22:47 +0200

    added all the files and maked all the buttons + styled

---
+++ script/script.js
@@ -0,0 +1,5 @@
+let buttonsNumbers = new Array(16);
+for(let i = 0; i<buttonsNumbers.length; i++){
+    buttonsNumbers[i].createElement("button");
+    buttonsNumbers[i].appendChild("container");
+}
